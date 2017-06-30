ba8fa2f0513a56c511c653132112526f8a5d1125
nikitoshom <nikitos_hom@mail.ru>
2017-06-30 15:16:51 +0300

    Update OutputNumbers.java

--- OutputNumbers.java
+++ OutputNumbers.java
@@ -1,10 +1,18 @@
 public class OutputNumbers {
   public static void main (String[] args) {
     for (int i = 0; i <= 100; i++) {
-      if (i % 3 == 0) {
-        System.out.println("3 * " + i / 3);
-      } else {
-        System.out.println(i);		
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("Tutti-Frutti");
+      } else {  
+        if (i % 3 == 0) {
+          System.out.println("Tutti");
+        } else {
+          if (i % 5 == 0) {
+            System.out.println("Frutti");
+          } else {
+            System.out.println(i);			
+          }
+        }
       }
     }
   }
