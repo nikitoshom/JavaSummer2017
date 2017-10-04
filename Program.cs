c80f69d64b879f7eb527a331e24a1e7b6e4f1f3a
nikitoshom <nikitos_hom@mail.ru>
2017-10-04 16:35:40 +0300

    DEV-11_Translit
    Files of DEV-11: ConvertString.cs, Program.cs.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DEV_11_Translit
+{
+  /*
+   * This program converting string, which was written in Cyrillica, into string was written in Latin and vice versa.
+   */
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ConvertString dictionary = new ConvertString();     
+      string str = Console.ReadLine();
+      str = dictionary.Transliter(str);
+      Console.WriteLine(str);
+      Console.ReadKey();      
+    }
+  }
+}
